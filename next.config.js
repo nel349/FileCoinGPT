dbf7e1c0494ca714328f39045d5d6ff84fa7e632
Norman Lopez <noell.lpz@gmail.com>
2023-06-17 21:10:31 -0700

    Added styling to my deal proposal form

--- next.config.js
+++ next.config.js
@@ -6,7 +6,19 @@ const nextConfig = {
     ALCHEMY_API_KEY_MAINNET: process.env.ALCHEMY_API_KEY_MAINNET,
     LIGHTHOUSE_API_KEY: process.env.LIGHTHOUSE_API_KEY,
     LIGHTHOUSE_API_KEY_DEPOT: process.env.LIGHTHOUSE_API_KEY_DEPOT,
-  }
+  },
+  // Enable CSS modules for all files
+  cssModules: true,
+  // Enable CSS modules for files with the .module.css extension
+  cssLoaderOptions: {
+    importLoaders: 1,
+    modules: {
+      mode: "local",
+      localIdentName: "[name]__[local]--[hash:base64:5]",
+      // Enable CSS modules only for files with the .module.css extension
+      test: /\.module\.css$/,
+    },
+  },
 }
 
 module.exports = nextConfig
--- src/components/DealProposalFormComponent.tsx
+++ src/components/DealProposalFormComponent.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { DealProposalParams } from "../fevm/make-deal-proposal";
+import styles from "./DealProposalForm.module.css";
 
 interface DealProposalFormProps {
   onSubmit: (params: DealProposalParams) => void;
@@ -9,7 +10,7 @@ export default function DealProposalForm({ onSubmit }: DealProposalFormProps) {
   const [contract, setContract] = useState("");
   const [pieceCid, setPieceCid] = useState("");
   const [pieceSize, setPieceSize] = useState(0);
-  const [verifiedDeal, setVerifiedDeal] = useState(0);
+  const [verifiedDeal, setVerifiedDeal] = useState(false);
   const [label, setLabel] = useState("");
   const [startEpoch, setStartEpoch] = useState(0);
   const [endEpoch, setEndEpoch] = useState(0);
@@ -45,83 +46,68 @@ export default function DealProposalForm({ onSubmit }: DealProposalFormProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
+    <form onSubmit={handleSubmit} className={styles["deal-proposal-form"]}>
+      <label className={styles["deal-proposal-form__label"]}>
         Contract:
-        <input type="text" value={contract} onChange={(e) => setContract(e.target.value)} />
+        <input type="text" value={contract} onChange={e => setContract(e.target.value)} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
+      <label className={styles["deal-proposal-form__label"]}>
         Piece CID:
-        <input type="text" value={pieceCid} onChange={(e) => setPieceCid(e.target.value)} />
+        <input type="text" value={pieceCid} onChange={e => setPieceCid(e.target.value)} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Piece Size:
-        <input type="number" value={pieceSize} onChange={(e) => setPieceSize(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Piece size:
+        <input type="number" value={pieceSize} onChange={e => setPieceSize(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Verified Deal:
-        <input type="number" value={verifiedDeal} onChange={(e) => setVerifiedDeal(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Verified deal:
+        <input type="checkbox" checked={verifiedDeal} onChange={e => setVerifiedDeal(e.target.checked)} className={styles["deal-proposal-form__checkbox"]} />
       </label>
-      <br />
-      <label>
+      <label className={styles["deal-proposal-form__label"]}>
         Label:
-        <input type="text" value={label} onChange={(e) => setLabel(e.target.value)} />
+        <input type="text" value={label} onChange={e => setLabel(e.target.value)} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Start Epoch:
-        <input type="number" value={startEpoch} onChange={(e) => setStartEpoch(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Start epoch:
+        <input type="number" value={startEpoch} onChange={e => setStartEpoch(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        End Epoch:
-        <input type="number" value={endEpoch} onChange={(e) => setEndEpoch(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        End epoch:
+        <input type="number" value={endEpoch} onChange={e => setEndEpoch(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Storage Price per Epoch:
-        <input type="number" value={storagePricePerEpoch} onChange={(e) => setStoragePricePerEpoch(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Storage price per epoch:
+        <input type="number" value={storagePricePerEpoch} onChange={e => setStoragePricePerEpoch(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Provider Collateral:
-        <input type="number" value={providerCollateral} onChange={(e) => setProviderCollateral(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Provider collateral:
+        <input type="number" value={providerCollateral} onChange={e => setProviderCollateral(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Client Collateral:
-        <input type="number" value={clientCollateral} onChange={(e) => setClientCollateral(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Client collateral:
+        <input type="number" value={clientCollateral} onChange={e => setClientCollateral(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Extra Params Version:
-        <input type="text" value={extraParamsVersion} onChange={(e) => setExtraParamsVersion(e.target.value)} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Extra params version:
+        <input type="text" value={extraParamsVersion} onChange={e => setExtraParamsVersion(e.target.value)} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Location Ref:
-        <input type="text" value={locationRef} onChange={(e) => setLocationRef(e.target.value)} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Location ref:
+        <input type="text" value={locationRef} onChange={e => setLocationRef(e.target.value)} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Car Size:
-        <input type="number" value={carSize} onChange={(e) => setCarSize(Number(e.target.value))} />
+      <label className={styles["deal-proposal-form__label"]}>
+        CAR size:
+        <input type="number" value={carSize} onChange={e => setCarSize(parseInt(e.target.value))} className={styles["deal-proposal-form__input"]} />
       </label>
-      <br />
-      <label>
-        Skip IPNI Announce:
-        <input type="checkbox" checked={skipIpniAnnounce} onChange={(e) => setSkipIpniAnnounce(e.target.checked)} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Skip IPNI announce:
+        <input type="checkbox" checked={skipIpniAnnounce} onChange={e => setSkipIpniAnnounce(e.target.checked)} className={styles["deal-proposal-form__checkbox"]} />
       </label>
-      <br />
-      <label>
-        Remove Unsealed Copy:
-        <input type="checkbox" checked={removeUnsealedCopy} onChange={(e) => setRemoveUnsealedCopy(e.target.checked)} />
+      <label className={styles["deal-proposal-form__label"]}>
+        Remove unsealed copy:
+        <input type="checkbox" checked={removeUnsealedCopy} onChange={e => setRemoveUnsealedCopy(e.target.checked)} className={styles["deal-proposal-form__checkbox"]} />
       </label>
-      <br />
-      <button type="submit">Submit</button>
+      <button type="submit" className={styles["deal-proposal-form__submit-button"]}>Submit</button>
     </form>
   );
 }
